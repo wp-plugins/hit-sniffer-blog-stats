1923e4db6b189b0cf05d7a77b02b566b4a439de0
sanarena <sanarena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-22 17:02:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hit-sniffer-blog-stats/trunk@303117 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsniffer.php
+++ hitsniffer.php
@@ -4,7 +4,7 @@ Plugin Name: Hit Sniffer Blog Stats
 Plugin URI: http://www.hitsniffer.com/
 Description: Hit Sniffer
 Author: hitsniffer.com
-Version: 1.9.8.7
+Version: 1.9.9
 Author URI: http://www.hitsniffer.com/
 */ 
 
