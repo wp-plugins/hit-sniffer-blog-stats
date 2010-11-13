a42eba0b771899c451c819060df159a539f28d6c
sanarena <sanarena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-13 17:39:51 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hit-sniffer-blog-stats/trunk@311132 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsniffer.php
+++ hitsniffer.php
@@ -10,11 +10,17 @@ Author URI: http://www.hitsniffer.com/
 
 add_action('admin_menu', 'hs_admin_menu');
 add_action('wp_footer', 'hitsniffer');
+add_action('wp_head', 'hitsniffer');
 
 
 
 function hitsniffer() {
-global $_SERVER,$_COOKIE;
+global $_SERVER,$_COOKIE,$hitsniffer_tracker;
+
+if ($hitsniffer_tracker){
+
+}else{
+$hitsniffer_tracker=1;
 
 $option=get_hs_conf();
 $option['code']=str_replace(" ","",html_entity_decode($option['code']));
@@ -118,6 +124,7 @@ htssc.parentNode.insertBefore(hstc, htssc);
 </script>
 <noscript><a href="http://www.hitsniffer.com/"><img src="<?php echo $purl; ?>hitsniffer.com/track.php?mode=img&amp;code=<?php echo substr($option['code'],0,32); ?><?php echo $htmlpar; ?>" alt="<?php echo $keyword[mt_rand(0,14)]; ?>" border='0' /><?php echo $keyword[mt_rand(0,14)]; ?></a></noscript>
 <!-- HITSNIFFER TRACKING CODE<?php echo $htssl; ?> - DO NOT CHANGE --><?php     
+}
 }
 
 
