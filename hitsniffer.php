a564b0b6286aceb14ddb8dc56b388ad2c4797b07
sanarena <sanarena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-07 16:55:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hit-sniffer-blog-stats/trunk@568703 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsniffer.php
+++ hitsniffer.php
@@ -4,7 +4,7 @@ Plugin Name: Hit Sniffer Live Blog Analytics
 Plugin URI: http://www.hitsniffer.com/
 Description: Hit Sniffer is a powerful real time website visitor activity tracker. It will  website visitors actions live and in real time.
 Author: hitsniffer.com
-Version: 2.7.1.3
+Version: 2.7.1.4
 Author URI: http://www.hitsniffer.com/
 */ 
 
